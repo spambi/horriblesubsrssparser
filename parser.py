39cbf8e2dd915679d4e81a11d1efb1fefeb28728
spambi <spambayyyy@gmail.com>
2019-08-27 12:57:04 +1000

    allows sorting through saved anime

--- parser.py
+++ parser.py
@@ -1,10 +1,9 @@
 import feedparser
-from deluge_client import DelugeRPCClient
-
 horriblesubsrss720 = "http://www.horriblesubs.info/rss.php?res=720"
-delugeClient = DelugeRPCClient('127.0.0.1', 12345, 'username', 'password')
+#client = Clinet()
 # List for anime currently watching, might change to a dic
-animeWatching = ['[HorribleSubs] Gegege no Kitarou (2018)']
+animeWatching = ['Arifureta Shokugyou de Sekai Saikyou']
+magnetLinks = []
 
 d = feedparser.parse(horriblesubsrss720)
 urls = []
@@ -18,3 +17,28 @@ for i in range(0, len(d.entries) - 1):
     urls.append(d.entries[i + 1].link)
 
 print urls[0]
+
+
+# Will remove tags for 480p and 720p RSS
+def removeTag(title):
+    # Will remove [Horrible Subs Tag]
+    editTitle1 = title[15:]
+    # Will remove file info
+    editTitle2 = editTitle1[:-16]
+    return editTitle1
+
+
+removeTag(urls[0])
+
+# Might change a to i and do something with limiting it to length of
+#  animeWatcihng Variable idk
+a = 0
+
+
+for i in range(0, len(urls)):
+    if animeWatching[a] in urls[i]:
+        magnetLinks.append(urls[i + 1])
+        if not a >= len(animeWatching):
+            i += 1
+
+print magnetLinks[0].encode('utf-8')
