5e339666dbfe3f80e5b7cfb9981e07750b7f3514
spambi <spamyyyy@gmail.com>
2020-02-20 22:16:21 +1000

    module works, integrate with main, ensure remote access enabled

---
+++ transmission_test.py
@@ -0,0 +1,4 @@
+import transmissionrpc
+
+tc = transmissionrpc.Client('localhost', port=9091)
+tc.add_torrent('magnet:?xt=urn:btih:UZJX6VV77P4SQVFRTT4HLK776V6DN74A&tr=http://nyaa.tracker.wf:7777/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://tracker.internetwarriors.net:1337/announce&tr=udp://tracker.leechersparadise.org:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://p4p.arenabg.com:1337/announce&tr=udp://mgtracker.org:6969/announce&tr=udp://tracker.tiny-vps.com:6969/announce&tr=udp://peerfect.org:6969/announce&tr=http://share.camoe.cn:8080/announce&tr=http://t.nyaatracker.com:80/announce&tr=https://open.kickasstracker.com:443/announce')
