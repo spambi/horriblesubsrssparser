5cd797dbf29b10d1f73d377b3914fdb4bb9c700f
spambi <spamyyyy@gmail.com>
2020-02-21 12:58:08 +1000

    Loop used titles length not total entries, also added error handling

--- parser.py
+++ parser.py
@@ -17,8 +17,13 @@ def removeTags(rssFeed):
     """Will remove tag from titles"""
     shows = []
     # Finds titles in RSS
-    for i in range(0, len(rssFeed.entries[-1].title)):
-        shows.append(rssFeed.entries[i].title)
+    try:
+        for i in range(0, len(d['entries'])):
+            shows.append(rssFeed.entries[i].title)
+            #print(rssFeed.entries[i].title_detail)
+    except IndexError:
+        print('Ended')
+        pass
 
     # Removes HS tags
     for i in range(0, len(shows)):
@@ -34,7 +39,7 @@ def findLinks(rssFeed):
     links = []
 
     # Finds links in RSS
-    for i in range(0, len(rssFeed.entries[-1].title)):
+    for i in range(0, len(rssFeed['entries'])):
         links.append(rssFeed.entries[i].link)
 
     return links
@@ -44,12 +49,16 @@ def categorizeLinks(rssFeed, shows, links):
     """Categorizes just title and remove [HorribleSubs tags]"""
     nonameAnime = []
     ballerAnime = []
-    for i in range(0, len(rssFeed.entries[-1].title)):
-        if animeWatching[0] in rssFeed.entries[i].title:
-            ballerAnime.append(rssFeed.entries[i].title)
-            ballerAnime.append(rssFeed.entries[i].link)
-        else:
-            nonameAnime.append(rssFeed.entries[i].title)
+    for i in range(0, len(rssFeed['entries'])):
+        try:
+            if animeWatching[0] in rssFeed.entries[i].title:
+                ballerAnime.append(rssFeed.entries[i].title)
+                ballerAnime.append(rssFeed.entries[i].link)
+            else:
+                nonameAnime.append(rssFeed.entries[i].title)
+        except IndexError:
+            print('Ended')
+            pass
     # print(ballerAnime)
     return ballerAnime
 
@@ -75,5 +84,9 @@ for i in range(0, len(bababoey)):
     if i % 2:
         print(bababoey[i-1])
         print(bababoey[i])
-    else:
+
         None
+
+#tc = transmissionrpc.Client('localhost', port=9091)
+#tc.get_torrents()
+#tc.add_torrent(bababoey[1])
